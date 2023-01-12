fbc866485893db2550d14a9b9850f72898dbad28
Gerry Williams <gerryw1389@gmail.com>
2023-01-12 08:08:51 -0600

    update

--- 2022-10-20-tf-new-subscription/main.tf
+++ 2022-10-20-tf-new-subscription/main.tf
@@ -9,8 +9,30 @@ locals {
   }
 }
 
-resource "azurerm_resource_group" "fa_rg" {
-  name     = "aa-${var.env_stage_abbr}-${var.region_abbr}-fa"
+resource "azurerm_resource_group" "hub_prod_rg" {
+  name     = "aa-${var.env_stage_abbr}-${var.region_abbr}-mgmt-rg"
   location = var.region
   tags     = local.aa_tags
+  provider = hub-prod
+}
+
+resource "azurerm_resource_group" "hub_nonprod_rg" {
+  name     = "aa-${var.env_stage_abbr}-${var.region_abbr}-mgmt-rg"
+  location = var.region
+  tags     = local.aa_tags
+  provider = hub-nonprod
+}
+
+resource "azurerm_resource_group" "spoke_prod_rg" {
+  name     = "aa-${var.env_stage_abbr}-${var.region_abbr}-mgmt-rg"
+  location = var.region
+  tags     = local.aa_tags
+  provider = spoke-prod
+}
+
+resource "azurerm_resource_group" "spoke_nonprod_rg" {
+  name     = "aa-${var.env_stage_abbr}-${var.region_abbr}-mgmt-rg"
+  location = var.region
+  tags     = local.aa_tags
+  provider = spoke-nonprod
 }
