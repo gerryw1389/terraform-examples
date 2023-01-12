d376046b7e4afa400b2c478eac7e58a1c6596834
Gerry Williams <gerryw1389@gmail.com>
2023-01-12 08:43:09 -0600

    update mgmt groups

--- 2022-10-20-tf-new-subscription/main.tf
+++ 2022-10-20-tf-new-subscription/main.tf
@@ -36,3 +36,46 @@ resource "azurerm_resource_group" "spoke_nonprod_rg" {
   tags     = local.aa_tags
   provider = azurerm.spoke-nonprod
 }
+
+### Mgmt Groups
+
+# Read in parent
+data "azurerm_management_group" "management" {
+name = "Automation Admin"
+}
+
+# Create child "Hub"
+resource "azurerm_management_group" "mgmt_hub" {
+display_name               = "Hub"
+parent_management_group_id = data.azurerm_management_group.management.id
+}
+
+resource "azurerm_management_group" "mgmt_hub_prod" {
+display_name               = "Prod"
+parent_management_group_id = azurerm_management_group.mgmt_hub.id
+subscription_ids           = [var.hub_prod_id]
+}
+
+resource "azurerm_management_group" "mgmt_hub_nonprod" {
+display_name               = "NonProd"
+parent_management_group_id = azurerm_management_group.mgmt_hub.id
+subscription_ids           = [var.hub_nonprod_id]
+}
+
+# Create child "Spoke"
+resource "azurerm_management_group" "mgmt_spoke" {
+display_name               = "Spoke"
+parent_management_group_id = data.azurerm_management_group.management.id
+}
+
+resource "azurerm_management_group" "mgmt_spoke_prod" {
+display_name               = "Prod"
+parent_management_group_id = azurerm_management_group.mgmt_spoke.id
+subscription_ids           = [var.spoke_prod_id]
+}
+
+resource "azurerm_management_group" "mgmt_spoke_nonprod" {
+display_name               = "NonProd"
+parent_management_group_id = azurerm_management_group.mgmt_spoke.id
+subscription_ids           = [var.spoke_nonprod_id]
+}
