035794d756ceec3c7ba3897cb06af633bbf435b2
Gerry Williams <gerryw1389@gmail.com>
2023-01-12 08:11:13 -0600

    add prefix

--- 2022-10-20-tf-new-subscription/main.tf
+++ 2022-10-20-tf-new-subscription/main.tf
@@ -13,26 +13,26 @@ resource "azurerm_resource_group" "hub_prod_rg" {
   name     = "aa-${var.env_stage_abbr}-${var.region_abbr}-mgmt-rg"
   location = var.region
   tags     = local.aa_tags
-  provider = hub-prod
+  provider = azurerm.hub-prod
 }
 
 resource "azurerm_resource_group" "hub_nonprod_rg" {
   name     = "aa-${var.env_stage_abbr}-${var.region_abbr}-mgmt-rg"
   location = var.region
   tags     = local.aa_tags
-  provider = hub-nonprod
+  provider = azurerm.hub-nonprod
 }
 
 resource "azurerm_resource_group" "spoke_prod_rg" {
   name     = "aa-${var.env_stage_abbr}-${var.region_abbr}-mgmt-rg"
   location = var.region
   tags     = local.aa_tags
-  provider = spoke-prod
+  provider = azurerm.spoke-prod
 }
 
 resource "azurerm_resource_group" "spoke_nonprod_rg" {
   name     = "aa-${var.env_stage_abbr}-${var.region_abbr}-mgmt-rg"
   location = var.region
   tags     = local.aa_tags
-  provider = spoke-nonprod
+  provider = azurerm.spoke-nonprod
 }
